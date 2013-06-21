8bfcb457d57b0081c8ae73e0b878ba8d1d847d68
Ethan Yuxuan Chen <ychen18@nd.edu>
2013-06-20 22:22:58 -0700

    Did the doPowerUp(). Change in typing.py was only for testing use.

--- typing.py
+++ typing.py
@@ -143,7 +143,7 @@ class MainPage(webapp2.RequestHandler):
                                    'initial_message': GameUpdater(game).get_game_message()
                                    }
                 print template_values
-                path = os.path.join(os.path.dirname(__file__), 'game_test.html')
+                path = os.path.join(os.path.dirname(__file__), 'game.html')
 
                 self.response.out.write(template.render(path, template_values))
             else:
